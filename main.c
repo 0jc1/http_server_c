e1dfe7bda84ef0b69680d1589827dfc84430171f
cookju <epicdevelopers26@gmail.com>
2024-04-20 21:12:13 -0400

    formatting

--- main.c
+++ main.c
@@ -1,43 +1,34 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <signal.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "server.h"
 
 #define VERSION 23
-#define PORT 8080 // default port
+#define PORT 8080  // default port
 #define BUFFER_SIZE 8096
 
-struct MimeType
-{
+struct MimeType {
     char *extension;
     char *type;
 } mimeTypes[] = {
-    {"gif", "image/gif"},
-    {"jpg", "image/jpg"},
-    {"jpeg", "image/jpeg"},
-    {"png", "image/png"},
-    {"css", "text/css"},
-    {"ico", "image/ico"},
-    {"zip", "image/zip"},
-    {"gz", "image/gz"},
-    {"tar", "image/tar"},
-    {"htm", "text/html"},
-    {"html", "text/html"},
+    {"gif", "image/gif"}, {"jpg", "image/jpg"},  {"jpeg", "image/jpeg"},
+    {"png", "image/png"}, {"css", "text/css"},   {"ico", "image/ico"},
+    {"zip", "image/zip"}, {"gz", "image/gz"},    {"tar", "image/tar"},
+    {"htm", "text/html"}, {"html", "text/html"},
 };
 
-enum HttpStatusCode
-{
+enum HttpStatusCode {
     OK = 200,
     CREATED = 201,
     ACCEPTED = 202,
@@ -54,26 +45,22 @@ enum HttpStatusCode
 HTTP_Server http_server;
 
 // Function to get MIME type based on file extension
-const char *getMimeType(const char *fileExtension)
-{
-    if (fileExtension == NULL)
-    {
+const char *getMimeType(const char *fileExtension) {
+    if (fileExtension == NULL) {
         return NULL;
     }
 
-    for (int i = 0; mimeTypes[i].extension != NULL; i++)
-    {
+    for (int i = 0; mimeTypes[i].extension != NULL; i++) {
         int len = strlen(mimeTypes[i].extension);
-        if (len > 0 && strncmp(fileExtension, mimeTypes[i].extension, len) == 0)
-        {
+        if (len > 0 &&
+            strncmp(fileExtension, mimeTypes[i].extension, len) == 0) {
             return mimeTypes[i].type;
         }
     }
     return NULL;
 }
 
-void logMessage(const char *format, ...)
-{
+void logMessage(const char *format, ...) {
     va_list arg;
     va_start(arg, format);
 
@@ -83,29 +70,25 @@ void logMessage(const char *format, ...)
     int len = vsnprintf(NULL, 0, format, argCopy);
     va_end(argCopy);
 
-    if (len < 0)
-    {
+    if (len < 0) {
         return;
     }
 
     char buf[len + 1];
     len = vsnprintf(buf, sizeof buf, format, arg);
-    if (len < 0)
-    {
+    if (len < 0) {
         return;
     }
 
     time_t t = time(NULL);
-    if (t == -1)
-    {
+    if (t == -1) {
         return;
     }
     struct tm *tm = localtime(&t);
-    if (tm == NULL)
-    {
+    if (tm == NULL) {
         return;
     }
-    char time_str[len + 13]; // Assuming HH:MM:SS format
+    char time_str[len + 13];  // Assuming HH:MM:SS format
     sprintf(time_str, "%02d:%02d:%02d ", tm->tm_hour, tm->tm_min, tm->tm_sec);
     strcat(time_str, buf);
 
@@ -113,7 +96,8 @@ void logMessage(const char *format, ...)
 
     // output to log file
     /*
-    int fd = open("server.log", O_WRONLY | O_APPEND | O_CREAT | O_NONBLOCK, S_IRUSR | S_IWUSR);
+    int fd = open("server.log", O_WRONLY | O_APPEND | O_CREAT | O_NONBLOCK,
+    S_IRUSR | S_IWUSR);
 
     if (fd == -1)
     {
@@ -134,14 +118,11 @@ void logMessage(const char *format, ...)
     */
 }
 
-void cleanup(int sig)
-{
-
+void cleanup(int sig) {
     printf("Cleaning up connections and exiting.\n");
 
     // try to close the listening socket
-    if (close(http_server.socket) < 0)
-    {
+    if (close(http_server.socket) < 0) {
         fprintf(stderr, "Error calling close()\n");
         exit(EXIT_FAILURE);
     }
@@ -150,12 +131,10 @@ void cleanup(int sig)
     exit(EXIT_SUCCESS);
 }
 
-char *render_static_file(char *fileName, long *len)
-{
+char *render_static_file(char *fileName, long *len) {
     FILE *file = fopen(fileName, "r");
 
-    if (file == NULL)
-    {
+    if (file == NULL) {
         return NULL;
     }
 
@@ -167,8 +146,7 @@ char *render_static_file(char *fileName, long *len)
     char *temp = malloc(sizeof(char) * (fsize + 1));
     char ch;
     int i = 0;
-    while ((ch = fgetc(file)) != EOF)
-    {
+    while ((ch = fgetc(file)) != EOF) {
         temp[i] = ch;
         i++;
     }
@@ -176,8 +154,7 @@ char *render_static_file(char *fileName, long *len)
     return temp;
 }
 
-void *handle_request(void *client_fd)
-{
+void *handle_request(void *client_fd) {
     int client_socket = *((int *)client_fd);
     char buffer[BUFFER_SIZE] = {0};
 
@@ -185,32 +162,25 @@ void *handle_request(void *client_fd)
 
     int ret = read(client_socket, buffer, BUFFER_SIZE - 1);
 
-    if (ret == 0 || ret == -1)
-    {
+    if (ret == 0 || ret == -1) {
         logMessage("failed to read client request");
         return NULL;
     }
 
-    if (ret > 0 && ret < BUFFER_SIZE)
-    {
+    if (ret > 0 && ret < BUFFER_SIZE) {
         buffer[ret] = 0;
-    }
-    else
-    {
+    } else {
         buffer[0] = 0;
     }
 
-    if (strncmp(buffer, "GET ", 4) && strncmp(buffer, "get ", 4))
-    {
+    if (strncmp(buffer, "GET ", 4) && strncmp(buffer, "get ", 4)) {
         logMessage("Only simple GET operation supported");
         return NULL;
     }
 
     // null terminate after the second space to ignore extra stuff
-    for (i = 4; i < BUFFER_SIZE; i++)
-    {
-        if (buffer[i] == ' ')
-        {
+    for (i = 4; i < BUFFER_SIZE; i++) {
+        if (buffer[i] == ' ') {
             buffer[i] = 0;
             break;
         }
@@ -221,18 +191,16 @@ void *handle_request(void *client_fd)
     enum HttpStatusCode statusCode = OK;
     char reasonPhrase[20] = "OK";
 
-    for (size_t j = 0; j < i - 1; j++)
-    {
+    for (size_t j = 0; j < i - 1; j++) {
         /* check for illegal parent directory use .. */
-        if (buffer[j] == '.' && buffer[j + 1] == '.')
-        {
+        if (buffer[j] == '.' && buffer[j + 1] == '.') {
             logMessage("Parent directory (..) path names not supported");
             statusCode = FORBIDDEN;
             strcpy(reasonPhrase, "Forbidden");
         }
     }
     // TODO show files in a directory
-    if (!strncmp(&buffer[0], "GET /\0", 6) || !strncmp(&buffer[0], "get /\0", 6)) /* convert no filename to index file */
+    if (!strncmp(&buffer[0], "GET /\0", 6) || !strncmp(&buffer[0], "get /\0",6)) /* convert no filename to index file */
         (void)strcpy(buffer, "GET /index.html");
 
     /* work out the file type and check we support it */
@@ -243,30 +211,31 @@ void *handle_request(void *client_fd)
     // Find the position of the dot
     char *dotPosition = strchr(&buffer[4], '.');
 
-    if (dotPosition != NULL)
-    {
+    if (dotPosition != NULL) {
         extension = dotPosition + 1;
     }
 
     fstr = getMimeType(extension);
 
-    if (fstr == 0)
-    {
+    if (fstr == 0) {
         logMessage("file extension type not supported");
     }
 
     char *file_name = &buffer[5];
     char *file_data = render_static_file(file_name, &len);
 
-    if (file_data == NULL)
-    {
+    if (file_data == NULL) {
         logMessage("failed to open file %s", &buffer[5]);
         statusCode = NOT_FOUND;
         strcpy(reasonPhrase, "Not Found");
     }
 
-    logMessage("SEND");                                                                                                                                                           /* lseek back to the file start ready for reading */
-    sprintf(buffer, "HTTP/1.1 %d %s\r\nServer: nweb/%d.0\r\nContent-Length: %ld\r\nConnection: close\r\nContent-Type: %s\r\n\r\n", statusCode, reasonPhrase, VERSION, len, fstr); /* Header + a blank line */
+    logMessage("SEND");
+    sprintf(buffer,
+            "HTTP/1.1 %d %s\r\nServer: nweb/%d.0\r\nContent-Length: "
+            "%ld\r\nConnection: close\r\nContent-Type: %s\r\n\r\n",
+            statusCode, reasonPhrase, VERSION, len,
+            fstr); /* Header + a blank line */
 
     (void)send(client_socket, buffer, strlen(buffer), 0);
 
@@ -278,34 +247,29 @@ void *handle_request(void *client_fd)
     return NULL;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int port = PORT;
     char *docroot = "docroot";
 
     // Parse the port number and doc root from the command-line argument
-    if (argc >= 2)
-    {
+    if (argc >= 2) {
         port = atoi(argv[1]);
-        if (argc >= 3)
-        {
-            docroot = argv[2];
-            if (chdir(docroot) == -1)
-            {
-                (void)printf("ERROR: Can't Change to directory %s\n", docroot);
-                exit(4);
-            }
+    }
+
+    if (argc >= 3) {
+        docroot = argv[2];
+        if (chdir(docroot) == -1) {
+            (void)printf("ERROR: Can't Change to directory %s\n", docroot);
+            exit(4);
         }
     }
 
-    if (docroot == NULL)
-    {
+    if (docroot == NULL) {
         printf("error: docroot is null\n");
         exit(EXIT_FAILURE);
     }
 
-    if (port <= 0 || port > 65535)
-    {
+    if (port <= 0 || port > 65535) {
         printf("Invalid port number: %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
@@ -320,18 +284,20 @@ int main(int argc, char *argv[])
 
     logMessage("Server listening on 127.0.0.1:%d...\n", port);
 
-    while (1)
-    {
+    while (1) {
         int *client_fd = malloc(sizeof(int));
 
         // Accept incoming connection
-        if ((*client_fd = accept(http_server.socket, (struct sockaddr *)&client_address, &http_server.address_len)) < 0)
-        {
+        if ((*client_fd =
+                 accept(http_server.socket, (struct sockaddr *)&client_address,
+                        &http_server.address_len)) < 0) {
             perror("Accept failed");
             exit(EXIT_FAILURE);
         }
 
-        logMessage("Connection accepted from %s:%d\n", inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
+        logMessage("Connection accepted from %s:%d\n",
+                   inet_ntoa(client_address.sin_addr),
+                   ntohs(client_address.sin_port));
 
         // Create a new thread to handle client request
         pthread_t thread_id;
--- server.c
+++ server.c
@@ -1,19 +1,17 @@
-#include "server.h"
-
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 
-void init_server(HTTP_Server *http_server, int port)
-{
+#include "server.h"
+
+void init_server(HTTP_Server *http_server, int port) {
     http_server->port = port;
 
     int server_socket, true1;
 
     // Create network socket
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -25,18 +23,16 @@ void init_server(HTTP_Server *http_server, int port)
     socklen_t address_len = sizeof(server_address);
 
     true1 = 1;
-    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &true1, sizeof(int)); // set option to reuse local addresses
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &true1, sizeof(int));  // set option to reuse local addresses
 
     // Bind the socket
-    if (bind(server_socket, (struct sockaddr *)&server_address, address_len) < 0)
-    {
+    if (bind(server_socket, (struct sockaddr *)&server_address, address_len) < 0) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
     // Listen for incoming connections
-    if (listen(server_socket, SOMAXCONN) < 0)
-    {
+    if (listen(server_socket, SOMAXCONN) < 0) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
